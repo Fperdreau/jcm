1b5b7eb31bdf2b0970760755326cb029686f30c2
Florian Perdreau <f.perdreau@donders.ru.nl>
2015-05-04 16:03:27 +0200

    New testing version of calendar 2.0

--- dev/calendar.js
+++ dev/calendar.js
@@ -2,16 +2,66 @@
  * Created by Florian on 08/03/2015.
  */
 
-$(document).ready(function() {
+var today = new Date();
+var firstMonth = today.getMonth();
+var year = today.getYear();
+
+function getCalendar(monthToShow, month, year) {
+
+    jQuery.ajax({
+        url: 'cal_process.php',
+        type: 'POST',
+        async: true,
+        data: {
+            getmonth: monthToShow,
+            month: month,
+            year: year
+        },
+        success: function(data){
+            var result = jQuery.parseJSON(data);
+            animate(op,result);
+        }
+    });
+}
+
+function animateCal(op, result) {
+    var move = (op == "prev") ? "+=":"-=";
     var wrapperleft = $('.cal_wrapper').position().left;
     var wrapperright = wrapperleft + $('.cal_wrapper').outerWidth();
     var corewidth = $('.cal_core').width();
 
+    if (totalwidth >= corewidth) {
+        $('.cal_month #'+monthtoremove).remove();
+    }
+
+    if (op === 'prev') {
+        $('.cal_core')
+            .css('left', 0)
+            .prepend(result)
+            .animate({
+                'left' : move+width
+            });
+    } else if (op === 'next') {
+        $('.cal_core')
+            .offset({left: 0})
+            .append(result)
+            .animate({
+                'left' : move+width
+            });
+    }
+}
+
+$(document).ready(function() {
+
+    var windowsWidth = $(document).width();
+    var nbMonthToShow = Math.round(windowsWidth/300);
+    getCalendar(nbMonthToShow,firstMonth,year);
+
     $('.mainbody')
+
         .ready(function() {
-            $('.cal_day').hide();
             $('.mainbody').find('.cal_day').each(function() {
-                    $(this).fadeIn(200);
+                    $(this).css('visibility','visible').fadeIn(100);
                 });
         })
 
@@ -19,21 +69,14 @@ $(document).ready(function() {
             var op = $(this).attr('id');
             var width = $('.cal_month').first().css('width');
             var firstmonth = $('.cal_month').first().attr('id');
-            var lastmonth = $('.cal_month').last().attr('id');
             var exploded = (op == 'prev') ? firstmonth.split('-') : lastmonth.split('-');
             var month_nb = exploded[0].split('_');
             var month = (op == "prev") ? parseInt(month_nb[1])-1:parseInt(month_nb[1])+1;
             var year = exploded[1];
-            var monthtoremove = (op == "prev") ? lastmonth:firstmonth;
-
-            var move = (op == "prev") ? "+=":"-=";
-            $('.cal_core').animate({
-                'left' : move+width
-            });
 
             var coreleft = $('.cal_core').position().left;
             var coreright = coreleft + $('.cal_core').outerWidth();
-            var add = coreleft <= wrapperleft || coreright >= wrapperright;
+
             if (coreleft >= wrapperleft) {
                 month = parseInt(month_nb[1])-1;
             } else if (coreright <= wrapperright) {
@@ -45,36 +88,9 @@ $(document).ready(function() {
                totalwidth += $(this).outerWidth();
             });
 
-            if (add == true) {
-                jQuery.ajax({
-                    url: 'cal_process.php',
-                    type: 'POST',
-                    async: true,
-                    data: {
-                        getmonth: true,
-                        month: month,
-                        year: year
-                    },
-                    success: function(data){
-                        var result = jQuery.parseJSON(data);
-                        if (totalwidth >= corewidth) {
-                            $('.cal_month #'+monthtoremove).remove();
-                        }
+            getCalendar(nbMonthToShow,month,year);
 
-                        if (op === 'prev') {
-                            $('.cal_core')
-                                .css('left', 0)
-                                .prepend(result);
-                        } else if (op === 'next') {
-                            $('.cal_core')
-                                .offset({left: 0})
-                                .append(result);
-                        }
-                    }
-                });
-            }
-
-        })
+        });
 
 
 });
