d7b8cd33b1c37444fb567b37ff263315afb810bc
Florian Perdreau <f.perdreau@donders.ru.nl>
2016-08-09 14:03:08 +0200

    added reminder and options to set when it should be sent

--- cronjobs/SpeakerAssignment.php
+++ cronjobs/SpeakerAssignment.php
@@ -43,7 +43,14 @@ class SpeakerAssignment extends AppCron {
     public $status = 'Off';
     public $installed = False;
     public static $description = "Automatically assigns speakers to the future sessions. Speaker are pseudo-randomly 
-    selected, with priority given to members with the least number of presentations given so far.";
+    selected with priority given to members with the least number of presentations given so far. This task also sends 
+    reminders to assigned speakers to remind them about their upcoming presentation. You can choose how many days before
+     the session the reminder should be sent.";
+    public $options = array(
+        'Days'=>array(
+            'options'=>array(),
+            'value'=>7)
+    );
 
     /**
      * Constructor
@@ -65,15 +72,56 @@ class SpeakerAssignment extends AppCron {
     
     /**
      * Run scheduled task: assign speaker to the next sessions
-     * @return bool|string
+     * @return string
      */
     public function run() {
         $Assignment = new autoAssignment($this->db);
-
+        if (!$Assignment->installed) {
+            return 'You must install the Assignment plugin first!';
+        }
         // Assign speakers
         $result = $Assignment->assign();
+        $log = $result['msg'];
+        $result = $this->send_reminders();
+        $log .= $result['msg'];
 
-        return $result['msg'];
+        return $log;
+    }
+
+    /**
+     * Send reminders to users about their upcoming presentation
+     * @return mixed
+     */
+    public function send_reminders() {
+        $result['status'] = true;
+        $result['msg'] = null;
+
+        $Assignment = new Session($this->db);
+        $MailManager = new MailManager($this->db);
+
+        // Get future sessions dates
+        $jc_days = $Assignment->getjcdates(1);
+        $session = new Session($this->db, $jc_days[0]);
+        $today = date('Y-m-d');
+        $dueDate = date('Y-m-d',strtotime($jc_days[0] . " - {$this->options['Days']['value']} day"));
+        var_dump($dueDate);
+        if ($today == $dueDate) {
+            $n = 0;
+            foreach ($session->presids as $presid) {
+                $Presentation = new Presentation($this->db, $presid);
+                $speaker = new User($this->db, $Presentation->username);
+                $content = $this->reminderEmail($speaker, array('date'=>$session->date, 'type'=>$session->type));
+                if ($MailManager->send($content, array($speaker->email))) {
+                    $result['status'] = true;
+                    $n++;
+                } else {
+                    $result['status'] = false;
+                    $result['msg'] = "Could not sent email to {$speaker->email}.";
+                }
+            }
+            $result['msg'] = "Reminder sent to {$n}";
+        }
+        return $result;
     }
 
     /**
@@ -120,6 +168,29 @@ class SpeakerAssignment extends AppCron {
             </div>
         ";
         $content['subject'] = "Invitation to present on the $date";
+        return $content;
+    }
+
+    /**
+     * Reminder email sent to user about his/her upcoming presentation
+     * @param User $user
+     * @param array $info
+     * @return mixed
+     */
+    public function reminderEmail(User $user, array $info) {
+        $sessionType = $info['type'];
+        $date = $info['date'];
+        $AppConfig = new AppConfig($this->db);
+        $contactURL = $AppConfig::$site_url."index.php?page=contact";
+        $content['body'] = "
+            <div style='width: 100%; margin: auto;'>
+                <p>Hello $user->fullname,</p>
+                <p>This is to remind you that you have a presentation (<span style='font-weight: 500'>$sessionType</span>) planned on the a <span style='font-weight: 500'>$date</span>.</p>
+                <p>Please, submit your presentation on the Journal Club Manager if you have not already.</p>
+                <p>If you think you will not be able to present on the assigned date, please <a href='$contactURL'>contact</a> on the organizers as soon as possible.</p>
+            </div>
+        ";
+        $content['subject'] = "Reminder: invitation to present on the {$date}";
         return $content;
     }
 }
