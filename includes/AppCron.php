a7be29188c892631575bcf4d9e8c0b88602f0b24
Florian Perdreau <f.perdreau@donders.ru.nl>
2016-10-30 17:52:24 +0100

    corrected plugSettings div alignment

--- includes/AppCron.php
+++ includes/AppCron.php
@@ -529,13 +529,14 @@ class AppCron extends AppTable {
     
                         <div class='settings'>
                             <form method='post' action='php/form.php'>
+                            
                                 <div class='plug_settings_panel'>
                                     <div>Date & Time</div>
-                                    <div class='form-group inline_field field_small'>
+                                    <div class='form-group field_small'>
                                         <input type='date' name='date' value='{$date}'/>
                                         <label>Date</label>
                                     </div>
-                                    <div class='form-group inline_field field_small'>
+                                    <div class='form-group field_small'>
                                         <input type='time' name='time' value='{$time}' />
                                         <label>Time</label>
                                     </div>
