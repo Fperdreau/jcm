85503a70c7dff3be2b12e75077e85f04acad25aa
Florian Perdreau <f.perdreau@donders.ru.nl>
2017-01-28 18:59:48 +0100

    Added about page

--- pages/about.php
+++ pages/about.php
@@ -1,16 +1,18 @@
 <section>
-    <h1>Journal Club Manager</h1>
-    <div class="subTitle">Organize your journal club efficiently</div>
-    <div class="version"><?php echo AppConfig::version; ?></div>
+    <div class="section_content">
+        <h1>Journal Club Manager</h1>
+        <div class="subTitle">Organize your journal club efficiently</div>
+        <div class="version">Version <?php echo AppConfig::version; ?></div>
+        <div class="">Sources:  <a href="<?php echo AppConfig::repository?>" target='_blank'><?php echo AppConfig::repository; ?></a>
+        <div>
+            <div class="title_lvl2">Support</div>
+            To report bugs or request new features, please go to: <a href="https://github.com/Fperdreau/jcm/issues" target="_blank">https://github.com/Fperdreau/jcm/issues</a>.
+        </div>
 
-    <div>
-        <h2>Description</h2>
-        <span class="app_name">Journal Club Manager</span> is a web-application improving the organization of journal clubs and other lab meetings.
-    </div>
-
-    <div>
-        <h2>License</h2>
-        <span class="app_name">Journal Club Manager</span>
-        <div>Copyright Florian Perdreau, &copy; 2014-2016 [<a href="http://www.gnu.org/licenses/">AGPL 3.0</a>]</div>
+        <div>
+            <div class="title_lvl2">License</div>
+            <span class="app_name">Journal Club Manager</span>
+            <div>Copyright Florian Perdreau, &copy; 2014-2016 [<a href="http://www.gnu.org/licenses/">AGPL 3.0</a>]</div>
+        </div>
     </div>
 </section>
