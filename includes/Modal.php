c59763d122ea2686dd059f4a55c02b29bb02f551
Florian Perdreau <f.perdreau@donders.ru.nl>
2017-04-17 10:03:33 +0200

    Added documentation

--- includes/Modal.php
+++ includes/Modal.php
@@ -1,10 +1,33 @@
 <?php
+/**
+ * File for class Users and User
+ *
+ * PHP version 5
+ *
+ * @author Florian Perdreau (fp@florianperdreau.fr)
+ * @copyright Copyright (C) 2014 Florian Perdreau
+ * @license <http://www.gnu.org/licenses/agpl-3.0.txt> GNU Affero General Public License v3
+ *
+ * This file is part of Journal Club Manager.
+ *
+ * Journal Club Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Journal Club Manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Journal Club Manager.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
- * Created by PhpStorm.
- * User: Florian
- * Date: 22/03/2017
- * Time: 10:00
+ * Class Modal
+ *
+ * Handles templates for modal windows and content router
  */
 class Modal {
 
