04b0eac910f595981762959282a8b57a12341459
Florian Perdreau <f.perdreau@donders.ru.nl>
2017-04-17 15:52:37 +0200

    Removed Uploads class and merged it into Media class

--- includes/Media.php
+++ includes/Media.php
@@ -24,25 +24,36 @@
  * along with Journal Club Manager.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class Uploads extends AppTable{
+class Media extends AppTable{
 
     protected $table_data = array(
         "id" => array("INT NOT NULL AUTO_INCREMENT", false),
         "date" => array('DATETIME', false),
         "fileid" => array('CHAR(20)', false),
         "filename" => array('CHAR(20)', false),
+        "name" => array('CHAR(255)', false),
         "presid" => array('CHAR(20)', false),
         "obj" => array('CHAR(255)', false),
         "type" => array('CHAR(5)', false),
         "primary" => 'id');
+
     protected $directory;
     protected $maxsize;
     protected $allowed_types;
 
+    public $presid;
+    public $fileid;
+    public $date;
+    public $filename;
+    public $name;
+    public $type;
+    public $obj;
+
     /**
      * Constructor
+     * @param null $file_id
      */
-    function __construct() {
+    function __construct($file_id=null) {
         parent::__construct('Media', $this->table_data);
         $this->directory = PATH_TO_APP.'/uploads/';
         $AppConfig = new AppConfig();
@@ -52,6 +63,11 @@ class Uploads extends AppTable{
         // Create uploads folder if it does not exist yet
         if (!is_dir($this->directory)) {
             mkdir($this->directory);
+        }
+
+        // Get file information
+        if (!is_null($file_id)) {
+            $this->getInfo($file_id);
         }
     }
 
@@ -79,9 +95,8 @@ class Uploads extends AppTable{
      */
     public function get_uploads($presid, $obj_name) {
         $uploads = array();
-        foreach ($this->db->select($this->tablename, array('fileid'), array('presid'=>$presid, 'obj'=>$obj_name)) as $key=>$item) {
-            $up = new Media($item['fileid']);
-            $uploads[$item['fileid']] = array('date'=>$up->date,'filename'=>$up->filename,'type'=>$up->type);
+        foreach ($this->db->select($this->tablename, array('*'), array('presid'=>$presid, 'obj'=>$obj_name)) as $key=>$item) {
+            $uploads[$item['fileid']] =$item;
         }
         return $uploads;
     }
@@ -95,10 +110,8 @@ class Uploads extends AppTable{
      */
     public function add_upload(array $file_names, $id, $obj_name=null) {
         if (is_null($obj_name)) $obj_name = get_called_class();
-
-        $upload = new Media();
         foreach ($file_names as $filename) {
-            if ($upload->add_presid($filename, $id, $obj_name) !== true) {
+            if ($this->add_presid($filename, $id, $obj_name) !== true) {
                 return False;
             }
         }
@@ -131,65 +144,41 @@ class Uploads extends AppTable{
     /**
      * Renders upload form
      * @param array $links
+     * @param string $id: uploader id (must be identical to the corresponding submission form)
      * @return string
      */
-    public static function uploader(array $links=array()) {
+    public static function uploader(array $links=array(), $id='uploader') {
         // Get files associated to this publication
         $filesList = "";
         if (!empty($links)) {
             foreach ($links as $fileid=>$info) {
-                $filesList .=
-                    "<div class='upl_info' id='upl_$fileid'>
-                        <div class='upl_name' id='$fileid'>$fileid</div>
-                        <div class='del_upl' id='$fileid' data-upl='$fileid'></div>
-                    </div>";
+                $filesList .= self::file_div($info);
             }
         }
 
+        $types = explode(',', AppConfig::getInstance()->upl_types);
+        $type_list = null;
+        foreach ($types as $type) {
+            $type_list .= "<div class='file_type'>{$type}</div>";
+        }
+
+
         $result = "
-        <div class='upl_container'>
+        <div class='upl_container' id='{$id}'>
            <div class='upl_errors'></div>
     	   <div class='upl_form'>
+                <div class='text'>Drop here</div>
                 <form method='post' enctype='multipart/form-data'>
                     <input type='file' name='upl' class='upl_input' multiple style='display: none;' />
                     <div class='upl_btn'>
-                        Add Files
-                        <br>(click or drop)
-                        <div class='upl_filetypes'>(" . AppConfig::getInstance()->upl_types . ")</div>
+                        <div>Browse</div>
+                        <div class='upl_file_types'>$type_list</div>
                     </div>
                 </form>
     	   </div>
            <div class='upl_filelist'>$filesList</div>
         </div>";
         return $result;
-    }
-
-}
-
-/**
- * Class Media
- *
- * Handles properties and methods specific to individual medium
- */
-class Media extends Uploads {
-
-    public $presid;
-    public $fileid;
-    public $date;
-    public $filename;
-    public $type;
-    public $obj;
-
-    /**
-     * Media constructor
-     * @param null $fileid
-    */
-    public function __construct($fileid=null){
-        parent::__construct();
-
-        if (!is_null($fileid)) {
-            $this->getInfo($fileid);
-        }
     }
 
     /**
@@ -201,7 +190,7 @@ class Media extends Uploads {
 
         // First check the file
         $result['error'] = $this->checkupload($file);
-        if ($result['error'] != true) {
+        if ($result['error'] !== true) {
             AppLogger::get_instance(APP_NAME, get_class($this))->error($result['error']);
             return $result;
         }
@@ -213,17 +202,48 @@ class Media extends Uploads {
             return $result;
         }
 
-        $this->date = date('Y-m-d h:i:s');
+        $data = array(
+            'date'=>date('Y-m-d h:i:s'),
+            'filename'=>$result['filename'],
+            'name'=>$result['name'],
+            'fileid'=>$result['file_id'],
+            'type'=>$result['type']
+        );
 
         // Third: add to the Media table
-        $class_vars = get_class_vars('Media');
-        $content = $this->parsenewdata($class_vars,array(),array('directory','maxsize','allowed_types'));
+        $content = $this->parsenewdata(get_class_vars(get_called_class()), $data,
+            array('directory','maxsize','allowed_types'));
         $result['error'] = $this->db->addcontent($this->tablename,$content);
         if ($result['error'] !== true) {
             $result['error'] = 'SQL: Could not add the file to the media table';
             AppLogger::get_instance(APP_NAME, get_class($this))->error($result['error']);
         }
-        return $result;
+        $data['error'] = $result['error'];
+        $data['input'] = self::hidden_input($data);
+        $data['file_div'] = self::file_div($data);
+        return $data;
+    }
+
+    /**
+     * Render hidden input
+     * @param array $data
+     * @return string
+     */
+    private static function hidden_input(array $data) {
+        return "<input type='hidden' class='upl_link' id='{$data['fileid']}' value='{$data['fileid']}' />";
+    }
+
+    /**
+     * Render file div (in uploader files list)
+     * @param array $data
+     * @return string
+     */
+    private static function file_div(array $data) {
+        $url = URL_TO_APP . '/uploads/' . $data['filename'];
+        return  "<div class='upl_info' id='upl_{$data['fileid']}'>
+                    <div class='upl_name'><a href='{$url}' target='_blank'>{$data['name']}</a></div>
+                    <div class='del_upl' id='{$data['fileid']}'></div>
+                </div>";
     }
 
     /**
@@ -354,22 +374,25 @@ class Media extends Uploads {
             $result['error'] = self::checkupload($file);
             if ($result['error'] === true) {
                 $tmp = htmlspecialchars($file['tmp_name'][0]);
-                $splitname = explode(".", strtolower($file['name'][0]));
-                $this->type = end($splitname);
+                $split_name = explode(".", strtolower($file['name'][0]));
+                $result['type'] = end($split_name);
 
                 // Create a unique filename
-                $newname = $this->makeId();
+                $file_info = $this->makeId($result['type']);
 
                 // Move file to the upload folder
-                $dest = $this->directory.$newname;
-                $result['error'] = move_uploaded_file($tmp,$dest);
+                $result['error'] = move_uploaded_file($tmp, $this->directory . $file_info['filename']);
 
                 if ($result['error'] == false) {
                     $result['error'] = "Uploading process failed";
                 } else {
                     $result['error'] = true;
-                    $result['status'] = $newname;
+                    $result['status'] = $file_info['filename'];
                 }
+                $name = explode('.', $file['name'][0]);
+                $result['name'] = $name[0];
+                $result['file_id'] = $file_info['fileid'];
+                $result['filename'] = $file_info['filename'];
             }
         } else {
             $result['error'] = "No File to upload";
@@ -377,15 +400,18 @@ class Media extends Uploads {
         return $result;
     }
 
-    public function makeId() {
+    /**
+     * Generate unique ID for file
+     * @param $type: file extension
+     * @return array: file information (id and name)
+     */
+    public function makeId($type) {
         $rnd = date('Ymd')."_".rand(0,100);
-        $newname = $rnd.".".$this->type;
-        while (is_file($this->directory.$newname)) {
+        $new_name = $rnd . '.' . $type;
+        while (is_file($this->directory . $new_name)) {
             $rnd = date('Ymd')."_".rand(0,100);
-            $newname = $rnd.".".$this->type;
+            $new_name = $rnd . '.' . $type;
         }
-        $this->fileid = $rnd;
-        $this->filename = $newname;
-        return $this->filename;
+        return array('fileid' => $rnd, 'filename'=>$new_name);
     }
 }
