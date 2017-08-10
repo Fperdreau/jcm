b2c6c16e69711bf92b1bff9c1038e7eda477e147
Florian Perdreau <f.perdreau@donders.ru.nl>
2017-08-10 13:39:21 +0200

    Fixed broken link

--- pages/error/401.php
+++ pages/error/401.php
@@ -2,8 +2,8 @@
     <div class='section_content'>
         <div style='color: rgb(105,105,105); font-size: 40px; text-align: center; font-weight: 600; margin-bottom: 20px;'>Sorry</div>
         <div style="color: rgb(105,105,105); text-align: center; font-size: 1.2em; font-weight: 400;">
-            You must <a class='leanModal' data-controller='User' data-action='get_view' data-params='login_form,modal' data-section='login_form'>
-            Sign In</a> or <a class='leanModal' data-controller='User' data-action='get_view' data-params='registration_form,modal' data-section='registration_form'>
+            You must <a class='leanModal' data-controller='Users' data-action='get_view' data-params='login_form,modal' data-section='login_form'>
+            Sign In</a> or <a class='leanModal' data-controller='Users' data-action='get_view' data-params='registration_form,modal' data-section='registration_form'>
             Sign Up</a> in order to access this page!
         </div>
     </div>
