e7536d12ce31c141d93b28c5907f3039d249ee70
Florian Perdreau <f.perdreau@donders.ru.nl>
2017-12-22 15:27:09 +0100

    Corrected url to verification page

--- includes/Users.php
+++ includes/Users.php
@@ -995,7 +995,7 @@ class Users extends BaseModel {
      * @return string
      */
     private static function activation_email($fullname, $email, $hash) {
-        $authorize_url =App::getAppUrl() . "index.php?page=verify&email={$email}&hash={$hash}&result=true";
+        $authorize_url = App::getAppUrl() . "index.php?page=organizer/verify&email={$email}&hash={$hash}&result=true";
         $newpwurl = App::getAppUrl() . "index.php?page=renew_pwd&hash={$hash}&email={$email}";
 
         return "
