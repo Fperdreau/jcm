720bdb0b9483347b2eedb7d01c23a25443c331b6
Florian Perdreau <f.perdreau@donders.ru.nl>
2018-01-07 07:28:04 +0100

    Added updateUserAvailability()

--- includes/Calendar.php
+++ includes/Calendar.php
@@ -37,4 +37,32 @@ class Calendar
         } catch (\Exception $e) {
         }
     }
+
+    public function updateUserAvailability($date)
+    {
+        $username = $_SESSION['username'];
+        //$date = $data['date'];
+        $Availability = new Availability();
+        $Presentation = new Presentation();
+
+        $result['status'] = $Availability->edit(array('date'=>$date, 'username'=>$username));
+        if ($result['status']) {
+            // Check whether user has a presentation planned on this day, if yes, then we delete it and notify the user that
+            // this presentation has been canceled
+            $data = $Presentation->get(array('date'=>$date, 'orator'=>$username));
+            if (!empty($data)) {
+                $speaker = new Users($username);
+                $Assignment = new Assignment();
+                $session = new Session($date);
+                $Presentation = new Presentation($data['id']);
+                $info['type'] = $session->type;
+                $info['date'] = $session->date;
+                $info['presid'] = $data['id'];
+                $result['status'] = $Presentation->delete_pres($data['id']);
+                if ($result['status']) {
+                    $result['status'] = $Assignment->updateAssignment($speaker, $info, false, true);
+                }
+            }
+        }
+    }
 }
