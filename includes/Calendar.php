3add467011fd0e387c6989cb58cf1a3fecdfc99e
Florian Perdreau <f.perdreau@donders.ru.nl>
2018-01-09 09:16:12 +0100

    Added getCalendarParams()

--- includes/Calendar.php
+++ includes/Calendar.php
@@ -24,6 +24,82 @@ class Calendar
     }
 
     /**
+     * Get calendar data
+     *
+     * @param bool $force_select
+     * @return array
+     * @throws Exception
+     */
+    private function getCalendarParams($force_select = true)
+    {
+        $formatdate = array();
+        $nb_pres = array();
+        $type = array();
+        $status = array();
+        $slots = array();
+        $session_ids = array();
+
+        foreach ($this->getSessionInstance()->all() as $session_id => $session_data) {
+            // Count how many presentations there are for this day
+            $nb = 0;
+            foreach ($session_data as $key => $data) {
+                $nb += !is_null($data['id_pres']) ? 1 : 0;
+            }
+            $type[] = $session_data[0]['type'];
+            $status[] = $session_data[0]['status'];
+            $slots[] = $session_data[0]['slots'];
+            $formatdate[] = date('d-m-Y', strtotime($session_data[0]['date']));
+            $nb_pres[] = $nb;
+            $session_ids[] = $session_id;
+        }
+
+        // Get user's availability and assignments
+        if (SessionInstance::isLogged()) {
+            $username = $_SESSION['username'];
+            $Availability = new Availability();
+            $availabilities = array();
+            foreach ($Availability->all(array('username'=>$username)) as $info) {
+                // Format date
+                $fdate = explode("-", $info['date']);
+                $day = $fdate[2];
+                $month = $fdate[1];
+                $year = $fdate[0];
+                $availabilities[] = "$day-$month-$year";
+            }
+
+            // Get user's assignments
+            $Presentation = new Presentation();
+            $assignments = array();
+            foreach ($Presentation->getList($username) as $row => $info) {
+                // Format date
+                $fdate = explode("-", $info['date']);
+                $day = $fdate[2];
+                $month = $fdate[1];
+                $year = $fdate[0];
+                $assignments[] = "$day-$month-$year";
+            }
+        } else {
+            $assignments = array();
+            $availabilities = array();
+        }
+
+        return array(
+            "Assignments"=>$assignments,
+            "Availability"=>$availabilities,
+            "max_nb_session"=>$this->getSessionInstance()->getSettings('max_nb_session'),
+            "jc_day"=>$this->getSessionInstance()->getSettings('jc_day'),
+            "today"=>date('d-m-Y'),
+            "booked"=>$formatdate,
+            "nb"=>$nb_pres,
+            "status"=>$status,
+            "slots"=>$slots,
+            "renderTypes"=>$type,
+            "force_select"=>$force_select,
+            "session_id"=>$session_ids
+        );
+    }
+
+    /**
      * Get calendar parameters
      *
      * @param boolean $force_select
@@ -32,7 +108,7 @@ class Calendar
     public function getParams($force_select = false)
     {
         try {
-            $result = $this->getSessionInstance()->getCalendarParams($force_select);
+            $result = $this->getCalendarParams($force_select);
             return $result;
         } catch (\Exception $e) {
         }
