6e61715f54a2cb338d5fa35f189b329570916e2f
Florian Perdreau <f.perdreau@donders.ru.nl>
2018-11-17 20:06:48 +0100

    Use camelCase naming convention

--- includes/Bookmark.php
+++ includes/Bookmark.php
@@ -10,7 +10,8 @@ use includes\BaseModel;
  * Date: 23/02/2017
  * Time: 08:19
  */
-class Bookmark extends BaseModel {
+class Bookmark extends BaseModel
+{
 
     public $id;
     public $ref_id;
@@ -20,7 +21,8 @@ class Bookmark extends BaseModel {
     /**
      * Constructor
      */
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct();
     }
 
@@ -29,12 +31,13 @@ class Bookmark extends BaseModel {
      * @param array $post
      * @return mixed
      */
-    public function add(array $post) {
+    public function add(array $post)
+    {
         if (SessionInstance::isLogged() && !$this->isExist(array('ref_id'=>$post['ref_id'], 'ref_obj'=>$post['ref_obj']))) {
             $post['username'] = $_SESSION['username'];
             $result['status'] = $this->db->insert($this->tablename, $this->parseData($post)) !== false;
             if ($result['status']) {
-                $info = $this->get_summary($post['ref_id'], $post['ref_obj'], $_SESSION['username']);
+                $info = $this->getSummary($post['ref_id'], $post['ref_obj'], $_SESSION['username']);
                 $result['state'] = $info['status'];
             }
         } else {
@@ -48,7 +51,8 @@ class Bookmark extends BaseModel {
      * @param array $post
      * @return mixed
      */
-    public function delete(array $post) {
+    public function delete(array $post)
+    {
         $result = array();
         if (SessionInstance::isLogged()) {
             $result['status'] = $this->db->delete($this->tablename, array('id'=>$post['id']));
@@ -66,7 +70,8 @@ class Bookmark extends BaseModel {
      * @param $username: username
      * @return array: array('count'=>int, 'status'=>boolean)
      */
-    public function get_summary($id, $ref_obj, $username) {
+    public function getSummary($id, $ref_obj, $username)
+    {
         return array(
             'status' => $this->isExist(array('ref_id'=>$id, 'ref_obj'=>$ref_obj, 'username'=>$username))
         );
@@ -79,7 +84,8 @@ class Bookmark extends BaseModel {
      * @param $username
      * @return string
      */
-    public function getIcon($id, $ref_obj, $username) {
+    public function getIcon($id, $ref_obj, $username)
+    {
         return self::show(
             array('ref_id'=>$id, 'ref_obj'=>$ref_obj),
             $this->isExist(array('ref_id'=>$id, 'ref_obj'=>$ref_obj, 'username'=>$username))
@@ -90,9 +96,10 @@ class Bookmark extends BaseModel {
      * @param $username
      * @return null|string
      */
-    public function getList($username) {
+    public function getList($username)
+    {
         $content = null;
-        foreach ($this->all(array('username'=>$username)) as $key=>$item) {
+        foreach ($this->all(array('username'=>$username)) as $key => $item) {
 
             /**
              * @var $Controller BaseModel
@@ -106,7 +113,9 @@ class Bookmark extends BaseModel {
             }
         }
 
-        if (is_null($content)) $content = self::nothing();
+        if (is_null($content)) {
+            $content = self::nothing();
+        }
         return $content;
     }
 
@@ -117,7 +126,8 @@ class Bookmark extends BaseModel {
      * @param bool $status : bookmarked or not by current user
      * @return string
      */
-    public static function show(array $data, $status) {
+    public static function show(array $data, $status)
+    {
         if ($status) {
             $css_icon = 'bookmark_on';
             $operation = 'delete';
@@ -126,7 +136,8 @@ class Bookmark extends BaseModel {
             $operation = 'add';
         }
         return "
-        <div class='tiny_icon bookmark_container {$css_icon}' data-controller='Bookmark' data-ref_id='{$data['ref_id']}' data-ref_obj='{$data['ref_obj']}' data-operation='{$operation}'>
+        <div class='tiny_icon bookmark_container {$css_icon}' data-controller='Bookmark' 
+        data-ref_id='{$data['ref_id']}' data-ref_obj='{$data['ref_obj']}' data-operation='{$operation}'>
         </div>";
     }
 
@@ -136,7 +147,8 @@ class Bookmark extends BaseModel {
      * @param array $data : target information
      * @return string
      */
-    public static function inList(array $bookmark, array $data) {
+    public static function inList(array $bookmark, array $data)
+    {
         $url = App::getAppUrl() . "index.php?page=". strtolower($bookmark['ref_obj']). "&id={$bookmark['ref_id']}";
         return "
             <div class='bookmark_list_container'>
@@ -154,10 +166,10 @@ class Bookmark extends BaseModel {
         ";
     }
 
-    private static function nothing() {
+    private static function nothing()
+    {
         return "
         <div class='bookmark_list_container'>Sorry, there is nothing to show here yet.</div>
         ";
     }
-
 }
--- includes/Vote.php
+++ includes/Vote.php
@@ -11,7 +11,8 @@ namespace includes;
 
 use includes\BaseModel;
 
-class Vote extends BaseModel {
+class Vote extends BaseModel
+{
 
     public $id;
     public $ref_id;
@@ -24,7 +25,8 @@ class Vote extends BaseModel {
      * @param array $post
      * @return array
      */
-    public function add(array $post) {
+    public function add(array $post)
+    {
         $result = array('status'=>false, 'msg'=>null);
         if (SessionInstance::isLogged() && !$this->isExist(array('ref_id'=>$post['ref_id'], 'ref_obj'=>$post['ref_obj'],
                 'username'=>$_SESSION['username']))) {
@@ -32,7 +34,7 @@ class Vote extends BaseModel {
             $post['username'] = $_SESSION['username'];
             $result['status'] = $this->db->insert($this->tablename, $this->parseData($post)) !== false;
             if ($result['status']) {
-                $info = $this->get_summary($post['ref_id'], $post['ref_obj'], $_SESSION['username']);
+                $info = $this->getSummary($post['ref_id'], $post['ref_obj'], $_SESSION['username']);
                 $result['count'] = $info['count'];
                 $result['state'] = $info['status'];
             }
@@ -47,7 +49,8 @@ class Vote extends BaseModel {
      * @param array $post
      * @return array
      */
-    public function delete(array $post) {
+    public function delete(array $post)
+    {
         $result = array('status'=>false, 'msg'=>null);
         if (SessionInstance::isLogged()) {
             $id = array(
@@ -57,22 +60,12 @@ class Vote extends BaseModel {
             );
             $result['status'] = $this->db->delete($this->tablename, $id);
             if ($result['status']) {
-                $info = $this->get_summary($post['ref_id'], $post['ref_obj'], $_SESSION['username']);
+                $info = $this->getSummary($post['ref_id'], $post['ref_obj'], $_SESSION['username']);
                 $result['count'] = $info['count'];
                 $result['state'] = $info['status'];
             }
         }
         return $result;
-    }
-
-    /**
-     * Get votes associated to an object
-     * @param string $id
-     * @param string $ref_obj
-     * @return array
-     */
-    public function get_votes($id, $ref_obj) {
-        return $this->get(array('ref_id'=>$id, 'ref_obj'=>$ref_obj));
     }
 
     /**
@@ -82,7 +75,8 @@ class Vote extends BaseModel {
      * @param $username: username
      * @return array: array('count'=>int, 'status'=>boolean)
      */
-    public function get_summary($id, $ref_obj, $username) {
+    public function getSummary($id, $ref_obj, $username)
+    {
         $data = $this->get(array('ref_id'=>$id, 'ref_obj'=>$ref_obj));
         return array(
             'count' => !empty($data) ? count($data) : 0,
@@ -97,8 +91,9 @@ class Vote extends BaseModel {
      * @param $username
      * @return string
      */
-    public function getIcon($id, $ref_obj, $username) {
-        $info = $this->get_summary($id, $ref_obj, $username);
+    public function getIcon($id, $ref_obj, $username)
+    {
+        $info = $this->getSummary($id, $ref_obj, $username);
         return self::show(array('ref_id'=>$id, 'ref_obj'=>$ref_obj, 'count'=>$info['count']), $info['status']);
     }
 
@@ -109,7 +104,8 @@ class Vote extends BaseModel {
      * @param $username
      * @return string
      */
-    public function getCount($id, $ref_obj, $username) {
+    public function getCount($id, $ref_obj, $username)
+    {
         $self = new self();
         $data = $self->get(array('ref_id'=>$id, 'ref_obj'=>$ref_obj));
         $vote = !empty($data) ? count($data) : 0;
@@ -125,7 +121,8 @@ class Vote extends BaseModel {
      * @param bool $status : liked or not by current user
      * @return string
      */
-    public static function show(array $data, $status) {
+    public static function show(array $data, $status)
+    {
         if ($status) {
             $css_icon = 'vote_liked';
             $operation = 'delete';
@@ -134,7 +131,8 @@ class Vote extends BaseModel {
             $operation = 'add';
         }
         return "
-        <div class='vote_container' data-controller='Vote' data-ref_id='{$data['ref_id']}' data-ref_obj='{$data['ref_obj']}' data-operation='{$operation}'>
+        <div class='vote_container' data-controller='Vote' data-ref_id='{$data['ref_id']}' 
+        data-ref_obj='{$data['ref_obj']}' data-operation='{$operation}'>
             <div class='tiny_icon vote_icon {$css_icon}'></div>
             <div class='vote_count'>{$data['count']}</div>
         </div>";
